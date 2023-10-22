56073604e388e8a897f0809afe0f97d6297b0a6e
shoegaze <7885170+shoegaze@users.noreply.github.com>
2023-10-22 14:40:56 +0900

    Adds `Screen` class

---
+++ Scripts/Screen.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace Corpo.Scripts; 
+
+public abstract class Screen : Node {
+  public abstract void OnFocus();
+  public abstract void OnCreate();
+  public abstract void OnDestroy();
+  
+  public abstract void Tick(float dt, GameInput? input);
+}
