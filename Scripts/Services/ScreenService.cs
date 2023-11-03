dcd1ae39411d819b9a716d8bff13ee0a1857fbe9
shoegaze <7885170+shoegaze@users.noreply.github.com>
2023-11-03 15:32:51 +0900

    Implements `ScreenService`

--- Scripts/Services/ScreenService.cs
+++ Scripts/Services/ScreenService.cs
@@ -1,7 +1,39 @@
-﻿namespace Corpo.Scripts.Services; 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+namespace Corpo.Scripts.Services; 
 
 public sealed class ScreenService : Service {
-  public ScreenService() {
-    // TODO(spike)
+  private Stack<Screen> Screens = new();
+
+  public Screen CurrentScreen => Screens.Any() ? Screens.Peek() : null;
+
+  public ScreenService() { }
+
+  public void Tick(float dt) {
+    CurrentScreen?.Tick(dt, GameInput.FromGlobal());   
+  }
+
+  public void Create(Screen screen) {
+    Screens.Push(screen);
+    screen.OnCreate();
+  }
+
+  public void Dismiss() {
+    if (Screens.Count == 0) {
+      GD.PrintErr("No screen to dismiss!");
+    }
+    
+    var previousScreen = Screens.Pop();
+    previousScreen.OnDismiss();
+
+    if (Screens.Count == 0) {
+      return;
+    }
+    
+    var currentScreen = Screens.Peek();
+    currentScreen.OnFocus();
   }
 }
