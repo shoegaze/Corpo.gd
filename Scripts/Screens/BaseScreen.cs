446fea0c86423002250238076cec775c8cab8954
shoegaze <7885170+shoegaze@users.noreply.github.com>
2023-11-03 13:02:56 +0900

    Adds `Screen` children stubs

---
+++ Scripts/Screens/BaseScreen.cs
@@ -0,0 +1,16 @@
+﻿namespace Corpo.Scripts.Screens; 
+
+public class BaseScreen : Screen {
+  public override void OnFocus() { }
+
+  public override void OnCreate() {
+    // TODO(spike): Show loading screen
+    // TODO(spike): Load packages
+  }
+
+  public override void OnDestroy() {
+    // TODO(spike): Dismiss loading screen
+  }
+  
+  public override void Tick(float dt, GameInput? input) { }
+}
