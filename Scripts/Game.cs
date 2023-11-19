ee7663b0e03c4610dd4e5674199fb750450fe540
shoegaze <7885170+shoegaze@users.noreply.github.com>
2023-11-19 19:43:17 +0900

    Adds environment JSON deserialization

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -2,13 +2,17 @@ using Corpo.Scripts.Services;
 using Corpo.Scripts.Services.Core;
 using Godot;
 
-namespace Corpo.Scripts; 
+namespace Corpo.Scripts;
 
 public partial class Game : Node {
   // Main entrypoint
   public override void _Ready() {
     ServiceProvider.BuildServices();
-    
+
+    EnvironmentService environmentService = ServiceProvider.Get<EnvironmentService>();
+    environmentService.LoadEnvironment(ProjectSettings.GlobalizePath("res://"),
+                                       EnvironmentService.EnvironmentMode.Development);
+
     StateService stateService = ServiceProvider.Get<StateService>();
     stateService.EnterState(GameState.Base);
   }
--- Scripts/Services/EnvironmentService.cs
+++ Scripts/Services/EnvironmentService.cs
@@ -2,33 +2,55 @@
 using System.IO;
 using Corpo.Scripts.Services.Core;
 using Godot;
+using Newtonsoft.Json;
 
-namespace Corpo.Scripts.Services; 
+namespace Corpo.Scripts.Services;
 
+// ReSharper disable once ClassNeverInstantiated.Global
 public class EnvironmentService : Service {
   // TODO(spike): Generate this
   public struct EnvironmentJson {
     // TODO
+    public string Foo { get; set; }
   }
 
   public enum EnvironmentMode {
     Development = 0,
     Production = 1
   }
-  
+
   public EnvironmentService() { }
 
   // Cannot be injected since this is a root Service
   private const string environmentFileNamePrefix = "environment";
   private const string environmentFileNameExtension = "json";
 
-  public EnvironmentJson? Environment;
+  public EnvironmentJson Environment { get; private set; }
 
-  public void LoadEnvironment(string fileName, EnvironmentMode mode) {
-    // TODO(spike):
-    // string fullFileName = $"{environmentFileNamePrefix}.{mode}.{environmentFileNameExtension}";
-    // File file = new File(fullFileName);
-    //
-    // Environment = Json.Parse<EnvironmentJson>(file.ToString());
+  private static string MapEnvironmentModeToFileNameFragment(EnvironmentMode mode) {
+    return mode switch {
+             EnvironmentMode.Development => "dev",
+             EnvironmentMode.Production => "prod",
+             _ => throw new Exception("Environment mode not supported")
+           };
+  }
+
+  public void LoadEnvironment(string rootPath, EnvironmentMode mode) {
+    string modeName = MapEnvironmentModeToFileNameFragment(mode);
+    string fullFileName = $"{environmentFileNamePrefix}.{modeName}.{environmentFileNameExtension}";
+    string fullFilePath = Path.Combine(rootPath, fullFileName);
+
+    GD.Print("Loading environment file ...");
+    GD.Print($" * {fullFilePath}");
+    
+    using StreamReader reader = new StreamReader(fullFilePath);
+    string jsonString = reader.ReadToEnd();
+
+    // TODO(spike): Validate JSON object
+    EnvironmentJson jsonObject = JsonConvert.DeserializeObject<EnvironmentJson>(jsonString);
+
+    Environment = jsonObject;
+    
+    GD.Print("> Complete!");
   }
 }
