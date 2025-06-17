805c444b4165c071f59dddce3a40947272f66442
shoegaze <7885170+shoegaze@users.noreply.github.com>
2025-06-17 13:42:27 +0900

    feat(engine): add ILogger definition

---
+++ src/Engine/ILogger.cs
@@ -0,0 +1,12 @@
+// ReSharper disable UnusedMember.Global
+
+namespace Engine;
+
+
+public interface ILogger {
+  void Debug(string message);
+  void Info(string message);
+  void Warn(string message);
+  void Error(string message);
+  void Fatal(string message);
+}
