4c24b0a18aff0f708509783800b21fdd5883409f
shoegaze <7885170+shoegaze@users.noreply.github.com>
2025-06-18 21:45:48 +0900

    test(game): add example test

---
+++ test/CorpoTest/test/ExampleTest.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable UnusedMember.Global
+
+using GdUnit4;
+
+
+namespace Corpo.CorpoTest;
+
+
+using static Assertions;
+
+
+[TestSuite]
+public class ExampleTest {
+  [TestCase]
+  public void SimpleAddition() {
+    AssertInt(1 + 1).IsEqual(2);
+  }
+}
