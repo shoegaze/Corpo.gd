41be009e42eb969cb929a8bb1fdc5d9ede696142
shoegaze <7885170+shoegaze@users.noreply.github.com>
2025-06-20 17:03:12 +0900

    build(game): Add development, staging, production build profiles

--- src/Corpo/Base/Environments/Models/Environment.cs
+++ src/Corpo/Base/Environments/Models/Environment.cs
@@ -1,52 +1,30 @@
-﻿#if DEBUG
-  #define ENV_DEV
-  #undef ENV_STG
-  #undef ENV_PROD
-
-#elif STAGING
-  #define ENV_STG
-  #undef ENV_DEV
-  #undef ENV_PROD
-
-#elif RELEASE
-  #define ENV_PROD
-  #undef ENV_DEV
-  #undef ENV_STG
-#endif
-
-using System;
-
-
 namespace Corpo.Base.Environments.Models;
 
 
 public static class Environment {
   public enum EnvironmentMode {
-    Development,
-    Staging,
-    Production
+	Development,
+	Staging,
+	Production
   }
 
   public static EnvironmentMode GetEnvironmentMode() {
-#if ENV_DEV
-    return EnvironmentMode.Development;
-#elif ENV_STAGING
-      return EnvironmentMode.Staging;
-#elif ENV_PROD
-      return EnvironmentMode.Production;
+#if DEVELOPMENT
+	return EnvironmentMode.Development;
+#elif STAGING
+	return EnvironmentMode.Staging;
+#elif PRODUCTION
+	return EnvironmentMode.Production;
 #endif
   }
 
   public static string GetEnvironmentModeAsName() {
-    EnvironmentMode mode = GetEnvironmentMode();
-
-    return mode switch {
-      EnvironmentMode.Development => "dev",
-      EnvironmentMode.Staging => "stg",
-      EnvironmentMode.Production => "prod",
-      _ => throw new ArgumentOutOfRangeException(
-        nameof(mode),
-        $"Invalid environment mode: {mode}")
-    };
+#if DEVELOPMENT
+	return "dev";
+#elif STAGING
+	return "stg";
+#elif PRODUCTION
+	return "prod";
+#endif
   }
 }
