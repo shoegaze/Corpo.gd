707ece5a57a0f6baf98f91829e50d755775d7936
shoegaze <7885170+shoegaze@users.noreply.github.com>
2025-09-10 20:54:30 +0900

    feat(prebuild): update environment paths schema

--- src/prebuild/packages/generate-json/assets/types/environment/environment.ts
+++ src/prebuild/packages/generate-json/assets/types/environment/environment.ts
@@ -1,50 +1,23 @@
-﻿import { object, string } from 'zod'
+﻿import { object } from 'zod'
+
+import { godotRelativePath, godotUrl } from '../godot'
 
 
-// TODO: Add string.refine(validatePath(...))
-//  const path = (root, path) => validatePath(root, path) ?? throw Error()
-//  const objRef = (screen, ref) => validateRef(screen, ref)
-export const environment =
+const paths = () =>
   object({
-    // TODO: Move paths def. under packages
-    path: object({
-      log: string(),
-      package: object({
-        root: string(),
-        shared: string(),
-        overworld: string(),
-        battle: string()
-      }),
-      screen: object({
-        base: string(),
-        loading: string(),
-        overworld: string(),
-        battle: string(),
-        mainMenu: object({
-          path: string(),
-          button: object({
-            root: string(),
-            newGame: string(),
-            loadGame: string(),
-            settings: string(),
-            exit: string()
-          }),
-          submenu: object({
-            root: string(),
-            saves: string(),
-            settings: string()
-          })
-        })
-      })
+    assets: object({
+      vars: godotUrl()
     }),
-    handle: object({
-      resource: object({
-        package: string(),
-        shared: string(),
-        // TODO: Merge into generic resource-group ID
-        overworld: string(),
-        battle: string()
+    file: object({
+      var: object({
+        config: godotRelativePath()
       })
     })
   })
-    .describe('Environment variables')
+
+
+export const environment =
+  object({
+    path: paths()
+  })
+    .describe('Environment JSON')
